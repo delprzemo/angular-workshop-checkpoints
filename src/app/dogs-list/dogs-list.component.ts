f131ec5be7edfb49da1c5291d0a13b77b41de11f
delprzemo <delprzemo@gmail.com>
2019-05-06 17:10:09 +0200

    Exercises 4,5

--- src/app/dogs-list/dogs-list.component.ts
+++ src/app/dogs-list/dogs-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DogsItem } from './models/dogs-item';
+import { DogListService } from './services/dog-list.service';
 
 @Component({
   selector: 'app-dogs-list',
@@ -8,15 +9,20 @@ import { DogsItem } from './models/dogs-item';
 })
 export class DogsListComponent {
   data: DogsItem[];
-  constructor() {
-    this.data = [];
-    this.data.push(new DogsItem(1, 'York', 'Yorks are very friendly dogs',
-    'https://c8.alamy.com/comp/JWWEFR/york-terrier-taking-a-bath-funny-wet-dog-JWWEFR.jpg'));
-    this.data.push(new DogsItem(2, 'German Shepard', 'German Shepard are very unfriendly dogs',
-    'https://i.ebayimg.com/images/g/lGYAAMXQNo5Taknl/s-l300.jpg'));
-    this.data.push(new DogsItem(3, 'Bulldog', 'Bulldog are very lazy dogs',
-    'https://i.ytimg.com/vi/ksEliwKxS-Y/hqdefault.jpg'));
-    this.data.push(new DogsItem(4, 'Lassie', 'Lessie are very old dogs',
-    'https://i.pinimg.com/736x/4d/c2/79/4dc279f7ad7fce4fc9ac65c4ed831bfd.jpg'));
+  constructor(private dogListService: DogListService) {
+    this.dogListService.buildDogList();
+    this.data = this.dogListService.dogs;
   }
+
+  checkIfIdEven(item: object): boolean {
+    return item['id'] % 2 === 0;
+  }
+
+  // getBackgroundColor(item: object): string {
+  //   if (item['id'] % 2 === 0) {
+  //     return 'lightgray';
+  //   } else {
+  //     return 'white';
+  //   }
+  // }
 }
